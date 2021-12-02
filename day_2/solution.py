6a62197d99d79d80a35c87af16e1c31a7f211497
drahcirD <richard.ericsson@outlook.com>
2021-12-02 09:10:02 +0100

    add day 2

---
+++ day_2/solution.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+import pathlib
+import fire
+
+def get_result():
+    def parse():
+        return [x.split(' ') for x in (pathlib.Path(__file__).resolve().parent / "input1.txt").read_text().split('\n')]
+        
+    def solve(data):
+        depth = 0
+        position = 0
+        for d in data:
+            direction, length = d
+            match direction:
+                case 'forward':
+                    position += int(length)
+                case 'down':
+                    depth += int(length)
+                case 'up':
+                    depth -= int(length)
+        return depth*position
+    
+    return solve(parse())
+
+def get_result2():
+    def parse():
+        return [x.split(' ') for x in (pathlib.Path(__file__).resolve().parent / "input2.txt").read_text().split('\n')]
+        
+    def solve(data):
+        depth = 0
+        position = 0
+        aim = 0
+        for d in data:
+            direction, length = d
+            match direction:
+                case 'forward':
+                    position += int(length)
+                    depth += int(length)*aim
+                case 'down':
+                    aim += int(length)
+                case 'up':
+                    aim -= int(length)
+        return depth*position
+    
+    return solve(parse())
+
+if __name__ == '__main__':
+  fire.Fire()
