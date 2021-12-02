4f4f80144fc9ee0e73696aec35c3c375aab0ce60
drahcirD <richard.ericsson@outlook.com>
2021-12-02 09:09:37 +0100

    Add day_1

---
+++ day_1/solution.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+import pathlib
+import fire
+import itertools
+from collections import Counter
+
+
+def get_result():
+    def parse():
+        return [int(x) for x in (pathlib.Path(__file__).resolve().parent / "input1.txt").read_text().split()]
+        
+    def solve(data):
+        return sum([1 for prev, next in itertools.pairwise(data) if next > prev])
+
+    
+    return solve(parse())
+
+def get_result2():
+    def parse():
+        return [int(x) for x in (pathlib.Path(__file__).resolve().parent / "input2.txt").read_text().split()]
+        
+    def solve(data):
+        temp = [sum([one, two, three]) for one, two, three in zip(data, data[1:], data[2:])]
+        return sum([1 for prev, next in itertools.pairwise(temp)  if next > prev])
+        
+    
+    return solve(parse())
+
+if __name__ == '__main__':
+  fire.Fire()
+    
